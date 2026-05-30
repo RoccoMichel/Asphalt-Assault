fecec1480934696cbdc81fe0f1a3a0c547852336
Algot2 <156318062+Algot2@users.noreply.github.com>
2026-05-30 20:51:22 +0200

    CarPhysisk V0.2

--- Assets/Scripts/CamraControler.cs
+++ Assets/Scripts/CamraControler.cs
@@ -13,9 +13,9 @@ public class CamraControler : MonoBehaviour
     }
     void Update()
     {
-        lukeat.position = Vector3.Lerp(lukeat.position, car.position + car.linearVelocity, Time.deltaTime * 50);
+        Vector3 vel = new Vector3(car.linearVelocity.x, 0, car.linearVelocity.z).normalized;
+        transform.position = Vector3.Lerp(transform.position, car.position - prodjectVecOnXZplabe(vel + car.transform.forward*0.5f) * 8 + Vector3.up*3, Time.deltaTime*50);
+        lukeat.position = Vector3.Lerp(lukeat.position, car.position + prodjectVecOnXZplabe(vel + car.transform.forward * 0.5f), Time.deltaTime * 50);
         transform.LookAt(lukeat);
-
-        transform.position = Vector3.Lerp(transform.position, car.position - prodjectVecOnXZplabe(car.linearVelocity.normalized + car.transform.forward*0.5f) * 8 + Vector3.up*3, Time.deltaTime*50);
     }
 }
--- Assets/Scripts/Car/CarFysiks.cs
+++ Assets/Scripts/Car/CarFysiks.cs
@@ -1,15 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CarFysiks : MonoBehaviour
 {
     public LayerMask Grownd;
     public float horspower,stering, grip;
+    public Transform[] wills;
     Rigidbody rb = new();
     float deltaAcselurashen { get { 
             return Input.GetAxisRaw("Vertical") 
                 * horspower 
                 * Time.deltaTime; } }
-    
+
+    float rotsensnForse
+    {
+        get
+        {
+            return Input.GetAxisRaw("Horizontal")
+                * stering
+                * Time.deltaTime * rb.linearVelocity.magnitude;
+        }
+    }
+
 
     public float GetSuspesenForse(Vector3 orienPos, float suspesenLegf, float stifnes) {
         float forse = 0;
@@ -36,8 +48,8 @@ public class CarFysiks : MonoBehaviour
         return line * alanment * vel.magnitude;
     }
     public float getVelosetyAlamentScore() {
-        Vector3 line = transform.forward;
-        Vector3 vel = rb.linearVelocity;
+        Vector3 line = new Vector3(transform.forward.x, 0, transform.forward.z);
+        Vector3 vel = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
 
         return Vector3.Dot(vel.normalized, line);
     }
@@ -47,10 +59,10 @@ public class CarFysiks : MonoBehaviour
     void Update() {
         if (GetSuspesenForse(transform.position, 1, 1) != 0) {
             rb.AddForce(transform.forward * deltaAcselurashen);
-            transform.rotation *= Quaternion.Euler(0f, Input.GetAxisRaw("Horizontal") * stering * Time.deltaTime * rb.linearVelocity.magnitude, 0f);
+            transform.rotation *= Quaternion.Euler(0f, rotsensnForse, 0f);
 
-            if (1 - getVelosetyAlamentScore() < grip)
-                rb.linearVelocity = ProdjectVelosetyOnForwordLine();
+            //if (1 - getVelosetyAlamentScore() < grip)
+            rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, ProdjectVelosetyOnForwordLine(), grip*Time.deltaTime/ rb.linearVelocity.magnitude);
         }
 
 
@@ -58,21 +70,33 @@ public class CarFysiks : MonoBehaviour
         float stifnes = 10000;
         float legf = 0.3f;
         float torkApliskesn = 0.5f;
+        int i = 0;
+        List<Vector3> WillPosisens = new List<Vector3> {
+            new Vector3(0.5f, -0.5f, 0.5f),
+            new Vector3(-0.5f, -0.5f, 0.5f),
+            new Vector3(0.5f, -0.5f, -0.5f),
+            new Vector3(-0.5f, -0.5f, -0.5f)
+        };
 
-        Vector3 LocSupsensenPosisen = new Vector3(0.5f, -0.5f, 0.5f);
-        Vector3 SupsensenPosisen = transform.TransformPoint(LocSupsensenPosisen);
-        rb.AddForceAtPosition(Vector3.up * GetSuspesenForse(SupsensenPosisen, legf, stifnes), transform.TransformPoint(LocSupsensenPosisen * torkApliskesn));
-        
-        LocSupsensenPosisen = new Vector3(-0.5f, -0.5f, 0.5f);
-        SupsensenPosisen = transform.TransformPoint(LocSupsensenPosisen);
-        rb.AddForceAtPosition(Vector3.up * GetSuspesenForse(SupsensenPosisen, legf, stifnes), transform.TransformPoint(LocSupsensenPosisen * torkApliskesn));
+        foreach (Vector3 LocSupsensenPosisen in WillPosisens) {
+          
+            float willRadios = 0.4f;
+            Vector3 SupsensenPosisen = transform.TransformPoint(LocSupsensenPosisen);
 
-        LocSupsensenPosisen = new Vector3(0.5f, -0.5f, -0.5f);
-        SupsensenPosisen = transform.TransformPoint(LocSupsensenPosisen);
-        rb.AddForceAtPosition(Vector3.up * GetSuspesenForse(SupsensenPosisen, legf, stifnes), transform.TransformPoint(LocSupsensenPosisen * torkApliskesn));
+            Vector3 willPos = SupsensenPosisen;
+            Ray spring = new Ray
+            {
+                direction = -transform.up,
+                origin = SupsensenPosisen
+            };
 
-        LocSupsensenPosisen = new Vector3(-0.5f, -0.5f, -0.5f);
-        SupsensenPosisen = transform.TransformPoint(LocSupsensenPosisen);
-        rb.AddForceAtPosition(Vector3.up * GetSuspesenForse(SupsensenPosisen, legf, stifnes), transform.TransformPoint(LocSupsensenPosisen * torkApliskesn));
+            if (Physics.Raycast(spring, out RaycastHit hit, legf, Grownd)) {
+                willPos = hit.point + Vector3.up * willRadios;
+            }
+
+            wills[i++].position = willPos;
+
+            rb.AddForceAtPosition(Vector3.up * GetSuspesenForse(SupsensenPosisen, legf, stifnes), transform.TransformPoint(LocSupsensenPosisen * torkApliskesn));
+        }
     }
 }
