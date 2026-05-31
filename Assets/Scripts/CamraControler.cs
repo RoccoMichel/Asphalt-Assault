d799acdddd3f10d3704be5c3d2124f9fc2993b74
Algot2 <156318062+Algot2@users.noreply.github.com>
2026-05-31 11:10:41 +0200

    Camra some out whif spede

--- Assets/Scripts/CamraControler.cs
+++ Assets/Scripts/CamraControler.cs
@@ -4,6 +4,7 @@ public class CamraControler : MonoBehaviour
 {
     public Rigidbody car;
     public Transform lukeat;
+    Camera cam;
     Vector3 prodjectVecOnXZplabe(Vector3 vec) {
         Vector3 vel = new Vector3(
         car.linearVelocity.x,
@@ -13,11 +14,16 @@ public class CamraControler : MonoBehaviour
 
         return vel.magnitude > 1f ? vel.normalized : car.transform.forward;
     }
+    private void Awake() {
+        cam = GetComponent<Camera>();
+    }
     void LateUpdate()
     {
         Vector3 vel = new Vector3(car.linearVelocity.x, 0, car.linearVelocity.z).normalized;
         transform.position = Vector3.Lerp(transform.position, car.position - prodjectVecOnXZplabe(vel + car.transform.forward*0.5f) * 5 + Vector3.up*3, Time.deltaTime * 12);
         lukeat.position = Vector3.Lerp(lukeat.position, car.position + prodjectVecOnXZplabe(vel + car.transform.forward * 0.5f)*4, Time.deltaTime * 12); 
         transform.LookAt(lukeat);
+
+        cam.fieldOfView = Mathf.Clamp(car.linearVelocity.magnitude*3, 60, 100);
     }
 }
