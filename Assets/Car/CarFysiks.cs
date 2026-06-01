619613bd2d97831d010761493084c23454fdc100
Algot2 <156318062+Algot2@users.noreply.github.com>
2026-06-01 17:53:46 +0200

    CarPhysisk V0.5

--- Assets/Car/CarFysiks.cs
+++ Assets/Car/CarFysiks.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CarFysiks : MonoBehaviour
 {
@@ -7,13 +8,21 @@ public class CarFysiks : MonoBehaviour
     public LayerMask ground;
     public float horsePower,boost,steering, grip, springDampening;
     public Transform[] wheels;
+    public Slider Slider;
     Rigidbody rb = new();
+    float _trust;
+    [HideInInspector] public float trust {
+        get => _trust;
+        set { 
+            _trust = Mathf.Clamp(value, 0, 1000); 
+            Slider.value = _trust; 
+        }
+    }
     float deltaAcceleration { get { 
             return Input.GetAxisRaw("Vertical") 
-                * (horsePower + (Input.GetKey(KeyCode.Space) ? boost : 0)) 
+                * horsePower 
                 * Time.fixedDeltaTime; } 
     }
-
     float rotationForce { get {
             return Input.GetAxisRaw("Horizontal")
                 * steering
@@ -22,8 +31,7 @@ public class CarFysiks : MonoBehaviour
                 * grip; }
     }
 
-
-    public float GetSuspensionForce(Vector3 oreginPos, float suspensionLenght, float stiffness) {
+    float GetSuspensionForce(Vector3 oreginPos, float suspensionLenght, float stiffness) {
         float force = 0;
         Ray spring = new Ray { 
             direction = -transform.up,
@@ -50,7 +58,7 @@ public class CarFysiks : MonoBehaviour
 
         return Mathf.Clamp(force * Time.fixedDeltaTime, 0, 1000);
     }
-    public Vector3 ProjectVelocityOnForwardLine() {
+    Vector3 ProjectVelocityOnForwardLine() {
         Vector3 line = transform.forward;
         Vector3 vel = rb.linearVelocity;
 
@@ -58,39 +66,16 @@ public class CarFysiks : MonoBehaviour
 
         return line * alignment * vel.magnitude;
     }
-    public float getVelocityAlignmentScore() {
+    float getVelocityAlignmentScore() {
         Vector3 line = new Vector3(transform.forward.x, 0, transform.forward.z);
         Vector3 vel = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
 
         return Vector3.Dot(vel.normalized, line);
     }
-    void Awake() {
-        rb = GetComponent<Rigidbody>();
-        rb.centerOfMass = new Vector3(0, -0.5f, 0);
-    }
-    void FixedUpdate() {
-        isGrounded = (GetSuspensionForce(transform.position, 1, 1) != 0);
-        if (isGrounded) {
-            rb.AddForce(transform.forward * deltaAcceleration);
-            rb.AddTorque(new Vector3(0f, rotationForce, 0f));
-
-            Vector3 forwardVel =
-            transform.forward *
-            Vector3.Dot(rb.linearVelocity, transform.forward); 
-            Vector3 sideVel =
-                transform.right *
-                Vector3.Dot(rb.linearVelocity, transform.right);
-
-            if (sideVel.magnitude > grip/2 || Input.GetKeyDown(KeyCode.LeftShift))
-                rb.linearVelocity = forwardVel + sideVel * (1f - grip * Time.fixedDeltaTime);
-            else rb.linearVelocity = forwardVel; 
-        }
-
-
-        // Suspension
+    void AplySuspensen() {
         float stiffness = 10000;
         float length = 0.3f;
-        float torqueApplication  = 0.5f;
+        float torqueApplication = 0.5f;
         int i = 0;
         List<Vector3> wheelPosition = new List<Vector3> {
             new Vector3(0.5f, -0.5f, 0.5f),
@@ -99,8 +84,9 @@ public class CarFysiks : MonoBehaviour
             new Vector3(-0.5f, -0.5f, -0.5f)
         };
 
-        foreach (Vector3 LocalSuspensionPosition in wheelPosition) {
-          
+        foreach (Vector3 LocalSuspensionPosition in wheelPosition)
+        {
+
             float wheelRadius = 0.4f;
             Vector3 suspensionPosition = transform.TransformPoint(LocalSuspensionPosition);
 
@@ -111,13 +97,53 @@ public class CarFysiks : MonoBehaviour
                 origin = suspensionPosition
             };
 
-            if (Physics.Raycast(spring, out RaycastHit hit, length, ground)) {
+            if (Physics.Raycast(spring, out RaycastHit hit, length, ground))
+            {
                 wheelPos = hit.point + Vector3.up * wheelRadius;
             }
 
             wheels[i++].position = wheelPos;
 
-            rb.AddForceAtPosition(transform.up * GetSuspensionForce(suspensionPosition, length, stiffness), transform.TransformPoint(LocalSuspensionPosition * torqueApplication ));
+            rb.AddForceAtPosition(transform.up * GetSuspensionForce(suspensionPosition, length, stiffness), transform.TransformPoint(LocalSuspensionPosition * torqueApplication));
         }
+    }
+    void ClampVelosetyToForwordDireksen() {
+        Vector3 forwardVel =
+              transform.forward *
+              Vector3.Dot(rb.linearVelocity, transform.forward);
+        Vector3 sideVel =
+            transform.right *
+            Vector3.Dot(rb.linearVelocity, transform.right);
+
+        if (sideVel.magnitude > grip / 2 || Input.GetKeyDown(KeyCode.LeftShift))
+            rb.linearVelocity = forwardVel + sideVel * (1f - grip * Time.fixedDeltaTime);
+        else rb.linearVelocity = forwardVel;
+    }
+    void AddTrost() {
+        rb.AddForce(transform.forward * boost);
+    }
+    void Awake() {
+        trust = 1000;
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = new Vector3(0, -0.5f, 0);
+    }
+    void FixedUpdate() {
+        isGrounded = (GetSuspensionForce(transform.position, 1, 1) != 0);
+
+        if (isGrounded)
+        {
+            rb.AddForce(transform.forward * deltaAcceleration);
+            rb.AddTorque(new Vector3(0f, rotationForce, 0f));
+
+            ClampVelosetyToForwordDireksen();
+        }
+        else trust++;
+
+        if (Input.GetKey(KeyCode.Space) && trust > 0) {
+            trust--;
+            AddTrost();
+        }
+
+        AplySuspensen();
     }
 }
--- Assets/Car/CarQaletyOflife.cs
+++ Assets/Car/CarQaletyOflife.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
-using UnityEngine.ProBuilder;
 
 public class CarQaletyOflife : MonoBehaviour
 {
     Rigidbody rb = new();
     CarFysiks fysiks = new();
     float ligerDamp, aglerDamp;
+    bool RotateInAre;
     void Awake(){
         rb = GetComponent<Rigidbody>();
         fysiks = rb.GetComponent<CarFysiks>();
@@ -33,11 +33,24 @@ public class CarQaletyOflife : MonoBehaviour
             RaycastHit hit = getNormolOfPontUnderCar();
             if (hit.normal != Vector3.zero) {
                 Vector3 TorqueAxes = Vector3.Cross(transform.up, hit.normal);
-                rb.AddTorque(TorqueAxes * Mathf.Clamp(5 - hit.distance, 0, 5), ForceMode.Acceleration);
+                rb.AddTorque(TorqueAxes * Mathf.Clamp(7.5f - hit.distance, 0, 7.5f), ForceMode.Acceleration);
             }
 
+            // are controle
+            //if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S)) 
+                RotateInAre = true;
+
+            if (RotateInAre && Input.GetAxisRaw("Vertical") != 0) { 
+                rb.AddTorque(transform.right * Input.GetAxisRaw("Vertical") * 15);
+                fysiks.trust--; 
+            }
+            Debug.DrawLine(transform.position, transform.position + transform.up * 100);
+            if (RotateInAre) { 
+                Debug.DrawLine(transform.position, transform.position + transform.forward*100);
+            }
         }
         else {
+            RotateInAre = false;
             rb.linearDamping = ligerDamp;
             rb.angularDamping = aglerDamp;
         }
